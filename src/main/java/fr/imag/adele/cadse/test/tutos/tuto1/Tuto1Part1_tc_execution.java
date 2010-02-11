2b3d1ce3e0f28fa647a8858c8afeb7eb332a988d
etienne <etienne@localhost>
2010-02-11 16:40:06 +0100

    (no commit message)

--- src/main/java/fr/imag/adele/cadse/test/tutos/tuto1/Tuto1Part1_tc_execution.java
+++ src/main/java/fr/imag/adele/cadse/test/tutos/tuto1/Tuto1Part1_tc_execution.java
@@ -4,16 +4,17 @@ import org.junit.Test;
 
 
 import fr.imag.adele.cadse.core.CadseGCST;
+import fr.imag.adele.cadse.test.tutos.common.TutoTestCase;
 import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.GTCadseFactory;
 import fr.imag.adele.graphictests.cadse.gtcadseworkbench_part.GTCadseShell;
 import fr.imag.adele.graphictests.cadse.test.GTCadseRTConstants;
-import fr.imag.adele.cadse.test.tutos.common.TutoTestCase;
+
 
 public class Tuto1Part1_tc_execution extends TutoTestCase {
 
 	@Test
 	public void test_selection() throws Exception {
-		
+	
 		// cadses selection
 		shell = new GTCadseShell(GTCadseRTConstants.CADSE_SELECTOR_SHELL_TITLE);
 		shell.selectCadses("Cadse Model.Workspace.WebAppModel");
