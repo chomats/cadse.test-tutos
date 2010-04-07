8bcb5de796ab60a5b41c7dcf865693c1670b6cd0
etienne <etienne@localhost>
2010-04-07 14:59:58 +0200

    tuto update

--- src/main/java/fr/imag/adele/cadse/test/tutos/tuto2/Tuto2Part1_ts_CADSEg.java
+++ src/main/java/fr/imag/adele/cadse/test/tutos/tuto2/Tuto2Part1_ts_CADSEg.java
@@ -1,6 +1,7 @@
 package fr.imag.adele.cadse.test.tutos.tuto2;
 
-
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,24 +11,13 @@ import fr.imag.adele.cadse.test.tutos.tuto1.Tuto1Part1_tc_CADSEg;
 import fr.imag.adele.cadse.test.tutos.tuto1.Tuto1Part2_tc_CADSEg;
 import fr.imag.adele.cadse.test.tutos.tuto1.Tuto1Part3_tc_CADSEg;
 import fr.imag.adele.cadse.test.tutos.tuto1.Tuto1Part4_tc_CADSEg;
-import fr.imag.adele.cadse.test.tutos.tuto1.Tuto1Part5_tc_CADSEg;
-import junit.framework.JUnit4TestAdapter;
-import junit.framework.Test;
-
 
 @RunWith(Suite.class)
-@SuiteClasses(value={
-		Tuto1Part1_tc_CADSEg.class,
-		Tuto1Part2_tc_CADSEg.class,
-		Tuto1Part3_tc_CADSEg.class,
-		Tuto1Part4_tc_CADSEg.class,
-		Tuto1Part5_tc_CADSEg.class,
-		Tuto2Part1_tc_CADSEg.class
-})
-
+@SuiteClasses(value = { Tuto1Part1_tc_CADSEg.class, Tuto1Part2_tc_CADSEg.class, Tuto1Part3_tc_CADSEg.class,
+		Tuto1Part4_tc_CADSEg.class, Tuto2Part1_tc_CADSEg.class })
 public class Tuto2Part1_ts_CADSEg {
 	public static Test suite() {
-		return (Test) new JUnit4TestAdapter(Tuto2Part1_ts_CADSEg.class);
+		return new JUnit4TestAdapter(Tuto2Part1_ts_CADSEg.class);
 	}
-	
+
 }
